939509946940937acc5da2d30777396f5ee9bb40
toufiq-dev <toufiq.dev@gmail.com>
2024-01-17 21:08:09 +0600

    Integration Stripe test

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -5,7 +5,7 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { catchError, map, Observable, tap, throwError } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { AUTH_SERVICE } from '../constants/services';
 import { UserDto } from '../dto';
 
@@ -17,9 +17,11 @@ export class JwtAuthGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
+
     if (!jwt) {
       return false;
     }
+
     return this.authClient
       .send<UserDto>('authenticate', {
         Authentication: jwt,
