2daa1cd957ec724cce5755ccca6a6445f8b0b7ea
toufiq-dev <toufiq.dev@gmail.com>
2024-01-18 10:11:55 +0600

    Initiating notifications microservice

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -12,9 +14,13 @@ export class PaymentsService {
     },
   );
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ card, amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
@@ -26,6 +32,8 @@ export class PaymentsService {
       },
       return_url: 'http://localhost:3000/reservations',
     });
+
+    this.notificationsService.emit('notify_email', { email });
 
     return paymentIntent;
   }
