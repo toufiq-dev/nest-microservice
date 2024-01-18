d39a54de7468fade558e21ae1d6384cb3f421466
toufiq-dev <toufiq.dev@gmail.com>
2024-01-18 23:17:14 +0600

    Deployment refactors

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -2,8 +2,8 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { CurrentUser } from '../../../../libs/common/src/decorators/current-user.decorator';
-import { UserDocument } from './models/user.schema';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { UserDocument } from '@app/common';
 
 @Controller('users')
 export class UsersController {
